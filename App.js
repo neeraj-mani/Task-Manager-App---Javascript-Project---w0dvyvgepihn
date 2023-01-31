be8a4251c916ae2f85dd1f3d71a516099388bcff
Neeraj Mani <87220853+neeraj-mani@users.noreply.github.com>
2023-01-31 23:16:53 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,61 @@
+const modal = document.querySelector(".task-modal");
+const modalBg = document.querySelector(".modal-bg");
+const createTaskBtn = document.querySelector(".create-task");
+const addTaskBtn = document.querySelector(".add-task");
+const openTasks = document.querySelector(".tasks__all");
+const taskInput = document.querySelector(".task-input");
+const taskContainerList = document.querySelectorAll(".tasks");
 
+createTaskBtn.addEventListener("click", (e) => {
+  modal.classList.toggle("modal-open");
+  taskInput.focus();
+});
+modalBg.addEventListener("click", () => {
+  modal.classList.toggle("modal-open");
+});
+function init() {}
+function addTask(taskString, taskType, taskContainer) {
+  taskContainer.insertAdjacentHTML(
+    "beforeend",
+    `
+      <div class="task-container" draggable="true">
+        <div class="task-label task-label__${taskType}"></div>
+        <p class="task-content">${taskString}</p>
+      </div>`
+  );
+}
+
+let currentDraggable = null;
+addTaskBtn.addEventListener("click", () => {
+  if (!taskInput.value.trim().length > 0) return;
+  addTask(taskInput.value, "open", openTasks);
+  modal.classList.toggle("modal-open");
+  taskInput.value = "";
+});
+taskContainerList.forEach((container) => {
+  container.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    // console.log(e);
+  });
+
+  container.addEventListener("drop", (e) => {
+    e.preventDefault();
+    if (e.target.closest(".tasks") === currentDraggable.parentElement) return;
+    const data = e.dataTransfer.getData("text");
+    addTask(
+      data,
+      e.target.closest(".tasks").dataset.type,
+      e.target.closest(".tasks")
+    );
+    currentDraggable.remove();
+  });
+
+  container.addEventListener("dragstart", (e) => {
+    currentDraggable = e.target;
+    e.dataTransfer.setData(
+      "text",
+      e.target.querySelector(".task-content").innerText
+    );
+    console.log(e);
+  });
+});
